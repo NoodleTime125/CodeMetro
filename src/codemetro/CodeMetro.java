68beebbb5a27c1734b829f5ad090c326a558bded
Kenneth Shen <Kenneth.Shen@alumni.ubc.ca>
2014-11-20 14:06:43 -0800

    Add error handling for invalid path.
    Conflicts:
    	src/codemetro/CodeMetro.java

--- src/codemetro/CodeMetro.java
+++ src/codemetro/CodeMetro.java
@@ -1,5 +1,6 @@
 package codemetro;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,12 +13,13 @@ import codemetro.analyzer.callgraph.parser.CallGraphParser;
 import codemetro.analyzer.gitinspector.GitInspector;
 import codemetro.analyzer.gitinspector.GitInspectorEntry;
 import codemetro.fuser.Fuser;
+import codemetro.visualizer.MapPlot;
 import de.fhpotsdam.unfolding.data.Feature;
 import de.fhpotsdam.unfolding.data.MultiFeature;
 
 public class CodeMetro {
 
-	
+
 	/**
 	 * Main method
 	 * @param args
@@ -26,33 +28,37 @@ public class CodeMetro {
 		//TODO Call both call graph and gitinspector.
 		System.out.println("Please input the path to the directory of the repository.");
 		Scanner scan = new Scanner(System.in);
-		
-		String repoRoot = scan.nextLine();
-		
-		Map <String, CallGraphNode> callGraph;
-		CallGraphParser cg = new CallGraphParser();
-		try {
-			callGraph = cg.generateGraph(repoRoot);
-		
-		
-		// Inspect our code and parse into an ArrayList
-		GitInspector gi1 = new GitInspector();
-		gi1.inspect(repoRoot);
-		ArrayList<GitInspectorEntry> c1 = gi1.parseOutput();
-		// Inspect other code and parse into an ArrayList
-//		GitInspector gi2 = new GitInspector();
-//		gi2.inspect("lib/codebase/Halja");
-//		ArrayList<GitInspectorEntry> c2 = gi2.parseOutput();
-		
-		//TODO Fuse both metrics into station/train.
-		Fuser f = new Fuser(c1, callGraph);
-		List<MultiFeature> feat = f.createTransit();
-		//TODO convert into visualizer input
-		//TODO visualize
-		System.out.println(feat);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
 
+		String repoRoot = scan.nextLine();
+		scan.close();
+		File root = new File(repoRoot);
+		if (!root.exists()){
+			System.err.println("Path to repository does not exist.");
+		} else {
+			Map <String, CallGraphNode> callGraph;
+			CallGraphParser cg = new CallGraphParser();
+			try {
+				callGraph = cg.generateGraph(repoRoot);
+
+
+				// Inspect our code and parse into an ArrayList
+				GitInspector gi1 = new GitInspector();
+				gi1.inspect(repoRoot);
+				ArrayList<GitInspectorEntry> c1 = gi1.parseOutput();
+				// Inspect other code and parse into an ArrayList
+
+				//TODO Fuse both metrics into station/train.
+				Fuser f = new Fuser(c1, callGraph);
+				List<MultiFeature> feat = f.createTransit();
+				//TODO convert into visualizer input
+				//TODO visualize
+				System.out.println(feat);
+				
+				new MapPlot(feat);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
 }
